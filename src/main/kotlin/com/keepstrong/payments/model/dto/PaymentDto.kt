a48bd72880f7e8d3703be620c4cbc44224b663e1
Vinicius R <vrodrigues.code@gmail.com>
2022-09-13 19:48:46 -0300

    added paymentdto

--- src/main/kotlin/com/keepstrong/payments/model/dto/PaymentDto.kt
+++ src/main/kotlin/com/keepstrong/payments/model/dto/PaymentDto.kt
@@ -1,4 +1,16 @@
 package com.keepstrong.payments.model.dto
 
-class PaymentDto {
-}
+import com.keepstrong.payments.model.entity.Status
+import java.math.BigDecimal
+
+data class PaymentDto(
+    private val id: Long,
+    private val value: BigDecimal,
+    private val name: String,
+    private val cardNumber: String,
+    private val expirationDate: String,
+    private val cardCode: String,
+    private val status: Status,
+    private val orderId: Long,
+    private val paymentMethodId: Long
+)
