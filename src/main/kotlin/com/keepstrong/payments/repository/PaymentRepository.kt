dab031b7200c15684f3c6aa2738b5d8fdcb726c6
Vinicius R <vrodrigues.code@gmail.com>
2022-09-13 19:49:09 -0300

    added payment repository

--- src/main/kotlin/com/keepstrong/payments/repository/PaymentRepository.kt
+++ src/main/kotlin/com/keepstrong/payments/repository/PaymentRepository.kt
@@ -1,4 +1,9 @@
 package com.keepstrong.payments.repository
 
-interface PaymentRepository {
+import com.keepstrong.payments.model.entity.Payment
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PaymentRepository : JpaRepository<Payment, Long> {
 }
