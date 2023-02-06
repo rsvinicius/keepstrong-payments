63407c1d3ca127382639c30cb45c87b50eab7ac8
Vinicius R <vrodrigues.code@gmail.com>
2023-02-06 16:01:57 -0300

    Removed Wildcard

--- src/main/kotlin/com/keepstrong/payments/model/entity/Payment.kt
+++ src/main/kotlin/com/keepstrong/payments/model/entity/Payment.kt
@@ -1,7 +1,13 @@
 package com.keepstrong.payments.model.entity
 
 import com.keepstrong.payments.model.Status
-import jakarta.persistence.*
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Positive
